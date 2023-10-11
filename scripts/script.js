feb9e6717a050cbba4481b9e0811e1a7d69e689b
Matheus Lima <guandira.ads@outlook.com>
2023-10-10 21:06:01 -0300

    add: Entregue exatamente igual. ainda da erro.

---
+++ scripts/script.js
@@ -0,0 +1,57 @@
+const emoji = [
+    "😸",
+    "😸",
+    "🦊",
+    "🦊",
+    "🐹",
+    "🐹",
+    "🐸",
+    "🐸",
+    "🐭",
+    "🐭",
+    "🦝",
+    "🦝",
+    "🐶",
+    "🐶",
+    "🐺",
+    "🐺"
+];
+let openCards = [];
+
+let embaralha = emoji.sort(()=>{
+        (Math.random() > 0.5 ? 2 : -1);
+    });
+
+for(let i=0; i<emoji.length;i++){
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML.embaralha[i];
+    box.onclick = handleClick;
+    document.querySelector(".game").appendChild(box);
+}
+
+function handleClick(){
+    if(openCards.length < 2){
+        this.classList.add("boxOpen");
+        openCards.push(this);
+    }
+    if(openCards.length == 2){
+        setTimeout(checkMatch, 500);
+    }
+}
+
+function checkMatch(){
+    if(openCards[0].innerHTML === openCards[1].innerHTML){
+        openCards[0].classList.add("boxMatch");
+        openCards[1].classList.add("boxMatch");
+    }
+    else{
+        openCards[0].classList.remove("boxOpen");
+        openCards[1].classList.remove("boxOpen");
+    }
+    openCards = [];
+
+    if(document.querySelectorAll(".boxMatch").length === emojis.length){
+        alert("VOCÊ VENCEU!");
+    }
+}
